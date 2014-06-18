waldenraines/katello

Fixes #5503/BZ1102315 - restrict UI interactions to actual permissions.

engines/bastion/app/assets/javascripts/bastion/auth/authorization.service.js
@@ -1,0 +1,45 @@
+/**
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the License (GPLv2) or (at your option) any later version.
+ * There is NO WARRANTY for this software, express or implied,
+ * including the implied warranties of MERCHANTABILITY,
+ * NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ * have received a copy of GPLv2 along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ **/
+
+/**
+ * @ngdoc service
+ * @name Bastion.auth:Authorization
+ *
+ * @requires CurrentUser
+ * @requires Permissions
+ *
+ * @description
+ *   A service for authorization related functionality.
+ */
+angular.module('Bastion.auth').service('Authorization', ['CurrentUser', 'Permissions', function (CurrentUser, Permissions) {
+
+    this.hasPermission = function (permissionName, model) {
+        var allowedTo = false;
+
+        if (CurrentUser.admin) {
+            allowedTo = true;
+        } else {
+            if (model && model.hasOwnProperty('permissions') && model.permissions.hasOwnProperty(permissionName)) {
+                allowedTo = model.permissions[permissionName];
+            } else {
+                angular.forEach(Permissions, function (permission) {
+                    if (permission.permission.name === permissionName) {
+                        allowedTo = true;
+                    }
+                });
+            }
+        }
+        return allowedTo;
+    };
+
+}]);

engines/bastion/app/assets/javascripts/bastion/bastion.module.js
@@ -147,6 +147,7 @@ angular.module('Bastion').config(
  * @requires $stateParams
  * @requires gettextCatalog
  * @requires currentLocale
+ * @requires Authorization
  * @requires $location
  * @requires $window
  * @requires PageTitle
@@ -155,8 +156,8 @@ angular.module('Bastion').config(
  * @description
  *   Set up some common state related functionality and set the current language.
  */
-angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextCatalog', 'currentLocale', '$location', '$window', 'PageTitle', 'RootURL',
-    function ($rootScope, $state, $stateParams, gettextCatalog, currentLocale, $location, $window, PageTitle, RootURL) {
+angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextCatalog', 'currentLocale', 'Authorization', '$location', '$window', 'PageTitle', 'RootURL',
+    function ($rootScope, $state, $stateParams, gettextCatalog, currentLocale, Authorization, $location, $window, PageTitle, RootURL) {
         var fromState, fromParams, orgSwitcherRegex;
 
         $rootScope.$state = $state;
@@ -219,5 +220,8 @@ angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextC
                 $window.location.href = newUrl;
             }
         });
+
+        $rootScope.permitted = Authorization.hasPermission;
+        $rootScope.denied = !Authorization.hasPermission;
     }
 ]);

engines/bastion/test/auth/authorization.service.test.js
@@ -1,0 +1,63 @@
+/**
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the License (GPLv2) or (at your option) any later version.
+ * There is NO WARRANTY for this software, express or implied,
+ * including the implied warranties of MERCHANTABILITY,
+ * NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ * have received a copy of GPLv2 along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+describe('Service:Authorization', function() {
+    var Authorization, CurrentUser, Permissions;
+
+    beforeEach(module('Bastion.auth', 'Bastion.test-mocks'));
+
+    beforeEach(module(function($provide) {
+        CurrentUser = {};
+        Permissions = [{permission: {name: 'view_tests'}}];
+
+        $provide.value('CurrentUser', CurrentUser);
+        $provide.value('Permissions', Permissions);
+    }));
+
+    beforeEach(inject(function(_Permissions_, _Authorization_) {
+        Permissions = _Permissions_;
+        Authorization = _Authorization_;
+    }));
+
+    describe("provides a function determines if the user has permission to perform an action", function () {
+        it("which returns true if the user is an admin", function () {
+            CurrentUser.admin = true;
+            expect(Authorization.hasPermission('randomPermission')).toBe(true);
+        });
+
+        describe("which checks for the permission in the provided model", function () {
+            var model = {permissions: {}};
+
+            it("and succeeds", function () {
+                model.permissions.view_tests = true;
+                expect(Authorization.hasPermission('view_tests', model)).toBe(true);
+            });
+
+            it("and fails", function () {
+                model.permissions.view_tests = false;
+                expect(Authorization.hasPermission('view_tests', model)).toBe(false);
+                expect(Authorization.hasPermission('view_results', model)).toBe(false);
+            });
+        });
+
+        describe("which checks if the permission name is in the Permissions list", function () {
+            it("and succeeds", function () {
+                expect(Authorization.hasPermission('view_tests')).toBe(true);
+            });
+
+            it("and fails", function () {
+                expect(Authorization.hasPermission('view_results')).toBe(false);
+            });
+        });
+    });
+});

Katello/bastion

app/assets/javascripts/bastion/auth/authorization.service.js
@@ -1,0 +1,48 @@
+/**
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the License (GPLv2) or (at your option) any later version.
+ * There is NO WARRANTY for this software, express or implied,
+ * including the implied warranties of MERCHANTABILITY,
+ * NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ * have received a copy of GPLv2 along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ **/
+
+/**
+ * @ngdoc service
+ * @name Bastion.auth:Authorization
+ *
+ * @requires CurrentUser
+ * @requires Permissions
+ *
+ * @description
+ *   A service for authorization related functionality.
+ */
+angular.module('Bastion.auth').service('Authorization', ['CurrentUser', 'Permissions', function (CurrentUser, Permissions) {
+
+    this.permitted = function (permissionName, model) {
+        var allowedTo = false;
+
+        if (CurrentUser.admin) {
+            allowedTo = true;
+        } else {
+            if (model && model.hasOwnProperty('permissions') && model.permissions.hasOwnProperty(permissionName)) {
+                allowedTo = model.permissions[permissionName];
+            } else {
+                angular.forEach(Permissions, function (permission) {
+                    if (permission.permission.name === permissionName) {
+                        allowedTo = true;
+                    }
+                });
+            }
+        }
+        return allowedTo;
+    };
+
+    this.denied = function (permissionName, model) {
+        return !this.permitted(permissionName, model);
+    };
+}]);

app/assets/javascripts/bastion/bastion.module.js
@@ -147,6 +147,7 @@ angular.module('Bastion').config(
  * @requires $stateParams
  * @requires gettextCatalog
  * @requires currentLocale
+ * @requires Authorization
  * @requires $location
  * @requires $window
  * @requires PageTitle
@@ -155,8 +156,8 @@ angular.module('Bastion').config(
  * @description
  *   Set up some common state related functionality and set the current language.
  */
-angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextCatalog', 'currentLocale', '$location', '$window', 'PageTitle', 'RootURL',
-    function ($rootScope, $state, $stateParams, gettextCatalog, currentLocale, $location, $window, PageTitle, RootURL) {
+angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextCatalog', 'currentLocale', 'Authorization', '$location', '$window', 'PageTitle', 'RootURL',
+    function ($rootScope, $state, $stateParams, gettextCatalog, currentLocale, Authorization, $location, $window, PageTitle, RootURL) {
         var fromState, fromParams, orgSwitcherRegex;
 
         $rootScope.$state = $state;
@@ -219,5 +220,8 @@ angular.module('Bastion').run(['$rootScope', '$state', '$stateParams', 'gettextC
                 $window.location.href = newUrl;
             }
         });
+
+        $rootScope.permitted = Authorization.permitted;
+        $rootScope.denied = Authorization.denied;
     }
 ]);

test/auth/authorization.service.test.js
@@ -1,0 +1,63 @@
+/**
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the License (GPLv2) or (at your option) any later version.
+ * There is NO WARRANTY for this software, express or implied,
+ * including the implied warranties of MERCHANTABILITY,
+ * NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ * have received a copy of GPLv2 along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+describe('Service:Authorization', function() {
+    var Authorization, CurrentUser, Permissions;
+
+    beforeEach(module('Bastion.auth', 'Bastion.test-mocks'));
+
+    beforeEach(module(function($provide) {
+        CurrentUser = {};
+        Permissions = [{permission: {name: 'view_tests'}}];
+
+        $provide.value('CurrentUser', CurrentUser);
+        $provide.value('Permissions', Permissions);
+    }));
+
+    beforeEach(inject(function(_Permissions_, _Authorization_) {
+        Permissions = _Permissions_;
+        Authorization = _Authorization_;
+    }));
+
+    describe("provides a function determines if the user has permission to perform an action", function () {
+        it("which returns true if the user is an admin", function () {
+            CurrentUser.admin = true;
+            expect(Authorization.permitted('randomPermission')).toBe(true);
+        });
+
+        describe("which checks for the permission in the provided model", function () {
+            var model = {permissions: {}};
+
+            it("and succeeds", function () {
+                model.permissions.view_tests = true;
+                expect(Authorization.permitted('view_tests', model)).toBe(true);
+            });
+
+            it("and fails", function () {
+                model.permissions.view_tests = false;
+                expect(Authorization.permitted('view_tests', model)).toBe(false);
+                expect(Authorization.permitted('view_results', model)).toBe(false);
+            });
+        });
+
+        describe("which checks if the permission name is in the Permissions list", function () {
+            it("and succeeds", function () {
+                expect(Authorization.permitted('view_tests')).toBe(true);
+            });
+
+            it("and fails", function () {
+                expect(Authorization.permitted('view_results')).toBe(false);
+            });
+        });
+    });
+});