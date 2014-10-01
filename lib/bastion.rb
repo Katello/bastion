484590687631aa556a1af0fc7c04c52b21b51649
Eric D. Helms <ericdhelms@gmail.com>
2014-10-01 15:54:13 +0100

    Refs #7423: Cleaning up less compilation.

--- lib/bastion.rb
+++ lib/bastion.rb
@@ -1,6 +1,11 @@
-require File.expand_path('bastion/engine', File.dirname(__FILE__))
-require 'less-rails'
+# to make Foreman#in_rake? helper available if Foreman's lib is available
+lib_foreman = File.expand_path('lib/foreman', Rails.root)
+require lib_foreman unless !Dir.exist?(lib_foreman)
+
+require 'less-rails' if !Rails.env.production? || Foreman.in_rake?
 require 'angular-rails-templates'
+
+require File.expand_path('bastion/engine', File.dirname(__FILE__))
 
 module Bastion
 
