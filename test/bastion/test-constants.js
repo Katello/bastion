586787653d8c4c7758e31021d4b32a57ba8aabcb
Walden Raines <walden@redhat.com>
2017-09-22 10:08:26 -0400

    Fixes #21076: add unregister_delete_host setting.
    http://projects.theforeman.org/issues/21076

--- test/bastion/test-constants.js
+++ test/bastion/test-constants.js
@@ -3,6 +3,8 @@ angular.module('Bastion').value('currentLocale', 'Here');
 angular.module('Bastion').value('CurrentOrganization', "ACME");
 angular.module('Bastion').value('Authorization', {});
 angular.module('Bastion').value('entriesPerPage', 20);
+angular.module('Bastion').value('deleteHostOnUnregister', false);
+angular.module('Bastion').value('markActiveMenu', function () {});
 angular.module('Bastion').value('PageTitle', 'Bastion Page');
 angular.module('Bastion').value('foreman', function () {});
 angular.module('Bastion').constant('BastionConfig', {
