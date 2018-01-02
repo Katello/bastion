fb8825ea173ed79e30550eacce87f0cba10465fb
Stephen Benjamin <stephen@redhat.com>
2018-01-02 18:05:32 +0000

    fixes #22118 - replace skip_before_filter with action for rails 5

--- app/controllers/bastion/bastion_controller.rb
+++ app/controllers/bastion/bastion_controller.rb
@@ -1,7 +1,7 @@
 module Bastion
   class BastionController < ::ApplicationController
     helper 'bastion/layout'
-    skip_before_filter :authorize
+    skip_before_action :authorize
 
     # prevent Foreman routes from being incorrectly generated (don't use the Bastion url_helpers)
     include Rails.application.routes.url_helpers
